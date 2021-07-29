5bae514da3227747e1c81d7cb29c80fa0f062275
aemkai <87898467+aemkai@users.noreply.github.com>
2021-07-29 15:57:12 +0200

    Update Clock.cpp

--- src/displayapp/screens/Clock.cpp
+++ src/displayapp/screens/Clock.cpp
@@ -163,7 +163,7 @@ Clock::Clock(DisplayApp* app,
 	lv_obj_set_style_local_radius(minLED2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_RADIUS_CIRCLE);
 	lv_obj_set_size(minLED2, LED_SIZE, LED_SIZE);
 //lv_obj_align(minLED2, minLED3, LV_ALIGN_OUT_RIGHT_MID, (LED_SIZE/2), 0);		
-lv_obj_align(minLED2, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 5, (5+(1.25*LED_SIZE));		// absolut setzen (LV_ALIGN_IN_LEFT_MID, 5, 5) ist i.O., wenn Zeit noch 20 px höher -> LV_ALIGN_IN_RIGHT_MID, 0, -35);)						   
+lv_obj_align(minLED2, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 5, (5+(1.25*LED_SIZE)));		// absolut setzen (LV_ALIGN_IN_LEFT_MID, 5, 5) ist i.O., wenn Zeit noch 20 px höher -> LV_ALIGN_IN_RIGHT_MID, 0, -35);)						   
 	
 	minLED1 = lv_obj_create(lv_scr_act(), nullptr);
 	lv_obj_set_style_local_bg_color(minLED1, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
