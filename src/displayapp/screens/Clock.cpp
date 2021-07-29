92394ccbf78de33fa05c42e10905f89f5c417ad0
aemkai <87898467+aemkai@users.noreply.github.com>
2021-07-29 14:21:05 +0200

    Update Clock.cpp

--- src/displayapp/screens/Clock.cpp
+++ src/displayapp/screens/Clock.cpp
@@ -133,7 +133,7 @@ Clock::Clock(DisplayApp* app,
 	lv_obj_set_style_local_bg_color(minLED5, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
 	lv_obj_set_style_local_radius(minLED5, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_RADIUS_CIRCLE);
 	lv_obj_set_size(minLED5, LED_SIZE, LED_SIZE);
-	lv_obj_align(minLED5, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 5, 5);
+	lv_obj_align(label_sec, label_time, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10); 	// relativ zu Uhrzeit setzen an linker unterer Kante, dann 10 px drunter
 	
 	minLED4 = lv_obj_create(lv_scr_act(), nullptr);
 	lv_obj_set_style_local_bg_color(minLED4, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
@@ -303,7 +303,7 @@ bool Clock::Refresh()
 	sprintf(secondsStr, "%c%c", secondsChar[0], secondsChar[1]);	  
 	lv_label_set_text(label_sec, secondsStr);
 
-	lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_CENTER, 0, -35);
+	lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_CENTER, 0, -60);
 	lv_obj_align(label_sec, label_time, LV_ALIGN_OUT_BOTTOM_MID, 85, 10);
 		  
 	 	  
