bde86df8fd9fc2e0791ad5f3c642d89480696424
aemkai <87898467+aemkai@users.noreply.github.com>
2021-08-01 21:43:17 +0200

    Update Clock.cpp

--- src/displayapp/screens/Clock.cpp
+++ src/displayapp/screens/Clock.cpp
@@ -161,7 +161,7 @@ Clock::Clock(DisplayApp* app,
 	minLED2 = lv_obj_create(lv_scr_act(), nullptr);
 	lv_obj_set_style_local_bg_color(minLED2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
 	lv_obj_set_style_local_radius(minLED2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_RADIUS_CIRCLE);
-lv_obj_set_style_local_border_width(minLED2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, 1);
+lv_obj_set_style_local_border_width(minLED2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, 3);
 lv_obj_set_style_local_line_color(minLED2,LV_BTN_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0x999999));						   
 	lv_obj_set_size(minLED2, LED_SIZE, LED_SIZE);
 //lv_obj_align(minLED2, minLED3, LV_ALIGN_OUT_RIGHT_MID, (LED_SIZE/2), 0);		
